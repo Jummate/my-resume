dcdda7ee963d573dac6521165dc31a46adf86f1b
Jummate <yakubjumat@gmail.com>
2021-08-22 04:26:20 +0100

    interface modified

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const _ = elem => {
+	return document.querySelector(elem);
+}
+
+let pressedToggle = true;
+
+//-------------This creates a rotaion effect on close and open menu toggler
+_("#hamburger-wrapper").addEventListener("click",() => {
+	if(pressedToggle)
+	{
+		_(".open-menu").setAttribute("class","fa fa-close open-menu");
+		_("#nav-smaller-screen").style.display = "block";
+		_(".open-menu").style.transform = "rotate(180deg)";
+		_(".open-menu").style.transition = "transform 0.7s ease";
+        _("#nav-smaller-screen").style.animation = "appear-slowly 0.7s ease";
+        _("body").style.overflowY = "hidden";
+		pressedToggle = false;
+	}
+	else
+	{
+		_(".open-menu").setAttribute("class","fa fa-bars open-menu");
+		_("#nav-smaller-screen").style.animation = "disappear-slowly 0.7s ease";
+		setTimeout(()=>{
+			_("#nav-smaller-screen").style.display = "none";
+		},500);
+		_(".open-menu").style.transform = "rotate(-180deg)";
+        _(".open-menu").style.transition = "transform 0.7s ease";
+         _("body").style.overflowY = "scroll";
+		pressedToggle = true;
+    }
+})
+
+_("#nav-smaller-screen").addEventListener("click", ()=>{
+    _(".open-menu").setAttribute("class","fa fa-bars open-menu");
+    _("#nav-smaller-screen").style.display = "none";
+    pressedToggle = true;
+});
